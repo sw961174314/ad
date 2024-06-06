5e9fbe606de8f6b4cfb2e6028208fdd12fb784d3
sw961174314 <961174314@qq.com>
2024-06-06 15:07:58 +0800

    2024.6.6 commit

--- ad-service/ad-common/src/main/java/com/ad/dump/DConstant.java
+++ ad-service/ad-common/src/main/java/com/ad/dump/DConstant.java
@@ -6,7 +6,7 @@ package com.ad.dump;
 public class DConstant {
 
     // 导出地址
-    public static final String DATA_ROOT_DIR = "/Users/mysql_data_/";
+    public static final String DATA_ROOT_DIR = "E:/Project/ad/mysql_data/";
 
     // 各个表数据的存储文件名
     public static final String AD_PLAN = "ad_plan.data";
